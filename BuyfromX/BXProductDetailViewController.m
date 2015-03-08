9369b48f159291114434d741b61296b2d5dc497d
Elliot Lee <github.public@intelliot.com>
2015-03-07 23:21:47 -0800

    Add BTDropInViewController

--- BuyfromX/BXProductDetailViewController.m
+++ BuyfromX/BXProductDetailViewController.m
@@ -2,7 +2,12 @@
 #import <AFNetworking/AFNetworking.h>
 #import <Braintree/Braintree.h>
 
-@interface BXProductDetailViewController ()
+@interface BXProductDetailViewController () <BTDropInViewControllerDelegate>
+
+@property (nonatomic, strong) Braintree *braintree;
+
+@property (nonatomic, copy) NSString *clientToken;
+@property (nonatomic, copy) NSString *nonce;
 
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 
@@ -13,16 +18,81 @@
 - (void)viewDidLoad {
     self.nameLabel.text = self.product[@"name"];
     
-//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-//    [manager GET:@"https://your-server/client_token.json"
-//      parameters:@{ @"your-server-authentication": @"token", @"your-customer-session": @"session"}
-//         success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//             // Setup braintree with responseObject[@"client_token"]
-//             self.braintree = [Braintree braintreeWithClientToken:responseObject[@"client_token"]];
-//         }
-//         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//             // Handle failure communicating with your server
-//         }];
+    self.clientToken = @"eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJhMmNjODhmMDQyZWI5NGM2ZTU5NGUzZDU3NjIzMmVhZDlmOTkxZDFiZDIwNTdjMmEwODgwY2IxNzY5ZDYyMzBhfGNyZWF0ZWRfYXQ9MjAxNS0wMy0wOFQwNzowNDo1Ni4zNDY1NTgwODkrMDAwMFx1MDAyNm1lcmNoYW50X2lkPWRjcHNweTJicndkanIzcW5cdTAwMjZwdWJsaWNfa2V5PTl3d3J6cWszdnIzdDRuYzgiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzL2RjcHNweTJicndkanIzcW4vY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIn0sInRocmVlRFNlY3VyZUVuYWJsZWQiOnRydWUsInRocmVlRFNlY3VyZSI6eyJsb29rdXBVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi90aHJlZV9kX3NlY3VyZS9sb29rdXAifSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwibWVyY2hhbnRBY2NvdW50SWQiOiJzdGNoMm5mZGZ3c3p5dHc1IiwiY3VycmVuY3lJc29Db2RlIjoiVVNEIn0sImNvaW5iYXNlRW5hYmxlZCI6dHJ1ZSwiY29pbmJhc2UiOnsiY2xpZW50SWQiOiIxMWQyNzIyOWJhNThiNTZkN2UzYzAxYTA1MjdmNGQ1YjQ0NmQ0ZjY4NDgxN2NiNjIzZDI1NWI1NzNhZGRjNTliIiwibWVyY2hhbnRBY2NvdW50IjoiY29pbmJhc2UtZGV2ZWxvcG1lbnQtbWVyY2hhbnRAZ2V0YnJhaW50cmVlLmNvbSIsInNjb3BlcyI6ImF1dGhvcml6YXRpb25zOmJyYWludHJlZSB1c2VyIiwicmVkaXJlY3RVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbS9jb2luYmFzZS9vYXV0aC9yZWRpcmVjdC1sYW5kaW5nLmh0bWwifSwibWVyY2hhbnRJZCI6ImRjcHNweTJicndkanIzcW4iLCJ2ZW5tbyI6Im9mZmxpbmUiLCJhcHBsZVBheSI6eyJzdGF0dXMiOiJtb2NrIiwiY291bnRyeUNvZGUiOiJVUyIsImN1cnJlbmN5Q29kZSI6IlVTRCIsIm1lcmNoYW50SWRlbnRpZmllciI6Im1lcmNoYW50LmNvbS5icmFpbnRyZWVwYXltZW50cy5kZXYtZGNvcGVsYW5kIiwic3VwcG9ydGVkTmV0d29ya3MiOlsidmlzYSIsIm1hc3RlcmNhcmQiLCJhbWV4Il19fQ==";
+    
+    if (!self.clientToken) {
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        [manager GET:@"https://your-server/client_token.json"
+          parameters:@{ @"your-server-authentication": @"token", @"your-customer-session": @"session"}
+             success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                 // Setup braintree with responseObject[@"client_token"]
+                 
+                 self.clientToken = responseObject[@"client_token"];
+                 
+                 
+                 
+                 
+                 
+             }
+             failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                 // Handle failure communicating with your server
+             }];
+    } else {
+        
+        
+        
+        //responseObject[@"client_token"]
+        
+        self.braintree = [Braintree braintreeWithClientToken:self.clientToken];
+    }
+}
+
+- (IBAction)buyItNowTapped:(id)sender {
+    // Create and retain a `Braintree` instance with the client token
+    self.braintree = [Braintree braintreeWithClientToken:self.clientToken];
+    // Create a BTDropInViewController
+    BTDropInViewController *dropInViewController = [self.braintree dropInViewControllerWithDelegate:self];
+    // This is where you might want to customize your Drop in. (See below.)
+    
+    // The way you present your BTDropInViewController instance is up to you.
+    // In this example, we wrap it in a new, modally presented navigation controller:
+    dropInViewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
+                                                                                                          target:self
+                                                                                                          action:@selector(userDidCancelPayment)];
+    
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:dropInViewController];
+    [self presentViewController:navigationController
+                       animated:YES
+                     completion:nil];
+}
+
+- (void)userDidCancelPayment {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)dropInViewController:(__unused BTDropInViewController *)viewController didSucceedWithPaymentMethod:(BTPaymentMethod *)paymentMethod {
+    self.nonce = paymentMethod.nonce;
+    [self postNonceToServer:self.nonce]; // Send payment method nonce to your server
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)dropInViewControllerDidCancel:(__unused BTDropInViewController *)viewController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (void)postNonceToServer:(NSString *)paymentMethodNonce {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:@"https://your-server/payment-method-nonce"
+       parameters:@{ @"your-server-authentication": @"token",
+                     @"your-customer-session": @"session",
+                     @"payment-method-nonce": paymentMethodNonce}
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              // Handle success
+          }
+          failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              // Handle failure communicating with your server
+          }];
 }
 
 @end
